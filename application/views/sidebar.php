6207f1252742a83930420dfc8e2450896b7ad25e
systpro04 <systemprogrammer04.com>
2023-10-06 08:27:08 +0800

    snow css sidebar

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -4,7 +4,7 @@
             style="margin-left: 20px">
     </a>
     <div class="sidebar mt-2">
-    <div id="snow-container"></div>
+        <div id="snow-container"></div>
         <div class="form-inline">
             <div class="input-group" data-widget="sidebar-search">
                 <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
@@ -18,7 +18,8 @@
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
-                    <a class="nav-link <?=(current_url()==base_url('dashboard')) ? 'active':''?>" href="<?=base_url('dashboard')?>">
+                    <a class="nav-link <?= (current_url() == base_url('dashboard')) ? 'active' : '' ?>"
+                        href="<?= base_url('dashboard') ?>">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Dashboard
@@ -26,7 +27,8 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link <?=(current_url()==base_url('data_view')) ? 'active':''?>" href="<?=base_url('data_view')?>">
+                    <a class="nav-link <?= (current_url() == base_url('data_view')) ? 'active' : '' ?>"
+                        href="<?= base_url('data_view') ?>">
                         <i class="nav-icon fas fa-align-right"></i>
                         <p>Adjustment</p>
                     </a>
@@ -37,96 +39,83 @@
 </aside>
 
 <style>
- main {
-  background: linear-gradient(to bottom, #2d91c2 0%, #1e528e 100%);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-family: "Pacifico", cursive;
-  height: 100vh;
-  padding: 20px;
-  text-align: center;
+main {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    padding: 20px;
+    text-align: center;
 }
 
 
-#snow-container {  
-  height: 100vh;
-  overflow: hidden;
-  position: absolute;
-  top: 0;
-  transition: opacity 500ms;
-  width: 100%;
+#snow-container {
+    height: 100vh;
+    overflow: hidden;
+    position: absolute;
+    top: 0;
+    transition: opacity 500ms;
+    width: 100%;
 }
 
 .snow {
-  animation: fall ease-in infinite, sway ease-in-out infinite;
-  color: skyblue;
-  position: absolute;
-}
-
-footer {
-  background-color: #ffdfb9;
-  bottom: 0;
-  font-family: sans-serif;
-  padding: 1rem;
-  text-align: center;
-  width: 100%;
-}
-
-footer a {
-  color: inherit;
-  text-decoration: none;
-}
-
-footer .heart {
-  color: #dc143c;
+    animation: fall ease-in infinite, sway ease-in-out infinite;
+    color: skyblue;
+    position: absolute;
 }
 
 @keyframes fall {
-  0% {
-    opacity: 0;
-  }
-  50% {
-    opacity: 1;
-  }
-  100% {
-    top: 100vh;
-    opacity: 1;
-  }
+    0% {
+        opacity: 0;
+    }
+
+    50% {
+        opacity: 1;
+    }
+
+    100% {
+        top: 100vh;
+        opacity: 0;
+    }
 }
 
 @keyframes sway {
-  0% {
-    margin-left: 0;
-  }
-  25% {
-    margin-left: 50px;
-  }
-  50% {
-    margin-left: -50px;
-  }
-  75% {
-    margin-left: 50px;
-  }
-  100% {
-    margin-left: 0;
-  }
+    0% {
+        margin-left: 0;
+    }
+
+    25% {
+        margin-left: 50px;
+    }
+
+    50% {
+        margin-left: -50px;
+    }
+
+    75% {
+        margin-left: 50px;
+    }
+
+    100% {
+        margin-left: 0;
+    }
 }
 </style>
+
 <script>
-    const snowContainer = document.getElementById("snow-container");
-const snowContent = ['&#10052', '&#10053', '&#10054']
+const snowContainer = document.getElementById("snow-container");
+const snowContent = ['&#10052', '&#10053', '&#10054'];
 
 const random = (num) => {
-  return Math.floor(Math.random() * num);
+    return Math.floor(Math.random() * num);
 }
 
 const getRandomStyles = () => {
-  const top = random(100);
-  const left = random(100);
-  const dur = random(10) + 10;
-  const size = random(25) + 25;
-  return `
+    const top = random(100);
+    const left = random(100);
+    const dur = random(10) + 10;
+    const size = random(30) + 20;
+    return `
     top: -${top}%;
     left: ${left}%;
     font-size: ${size}px;
@@ -135,16 +124,16 @@ const getRandomStyles = () => {
 }
 
 const createSnow = (num) => {
-  for (var i = num; i > 0; i--) {
-    var snow = document.createElement("div");
-    snow.className = "snow";
-    snow.style.cssText = getRandomStyles();
-    snow.innerHTML = snowContent[random(3)]
-    snowContainer.append(snow);
-  }
+    for (var i = num; i > 0; i--) {
+        var snow = document.createElement("div");
+        snow.className = "snow";
+        snow.style.cssText = getRandomStyles();
+        snow.innerHTML = snowContent[random(3)];
+        snowContainer.append(snow);
+    }
 }
 
 window.addEventListener("load", () => {
-  createSnow(30)
+    createSnow(30);
 });
 </script>
